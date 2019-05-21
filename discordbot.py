b512979ce53a0c2379b3b8e98934931fd2fb77e9
jorge martinez <martinezjorge966@live.com>
2019-05-21 05:46:55 -0400

    new mongo integration and new mongo function

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,17 @@
+from discord import Client
+
 import config
 import requests
 import json
-from discord.ext import commands , tasks
+from discord.ext import commands, tasks
 import time as time
 import mongoDriver as md
-import pprint as pprint
-matchIdDb = md.mongoConnection("127.0.0.1", "matchDatabase", "matches")
+import pprint as pp
+
+acctIdDb = md.mongoConnection("127.0.0.1", "matchDatabase", "Users")
+lastMatchDb = md.mongoConnection("127.0.0.1", "matchDatabase", "lastMatches")
+
+
 
 
 
@@ -16,9 +22,115 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
 
+@bot.command()
+async def gr(ctx, arg):
+    isAccountId = False
+    try:
+        input = int(arg)
+        isAccountId = True
+    except:
+        pass
 
-#@bot.command()
-#async def gr2(ctx, input):
+    if isAccountId:
+        recent = requests.get("https://api.opendota.com/api/players/" + str(arg) + "/recentMatches")
+        await ctx.send("getting recent matches...")
+        time.sleep(2)
+        json_data = json.loads(recent.text)
+        match_id = json_data[0]['match_id']
+        url = "https://api.opendota.com/api/matches/" + str(match_id)
+        await ctx.send("getting match...")
+        time.sleep(2)
+        data = requests.get(url)
+        json_data2 = json.loads(data.text)
+        players = json_data2["players"]
+        temp = None
+        temp_hero = None
+
+        for item in players:
+            if int(arg) == item["account_id"]:
+                temp = item
+        persona_name = temp["personaname"]
+        radiant_win = temp["radiant_win"]
+        is_radiant = temp["isRadiant"]
+        hero_id = temp["hero_id"]
+
+        get_hero = requests.get("https://api.opendota.com/api/heroes")
+        load_hero = json.loads(get_hero.text)
+
+        for item in load_hero:
+            if hero_id == item["id"]:
+                temp_hero = item["localized_name"]
+
+        if radiant_win:
+            if is_radiant:
+                await ctx.send(persona_name + " won their last game" + " as " + temp_hero)
+            else:
+                await ctx.send(persona_name + " lost their last game" + " as " + temp_hero)
+        elif not radiant_win:
+            if not is_radiant:
+                await ctx.send(persona_name + " won their last game" + " as " + temp_hero)
+            else:
+                await ctx.send(persona_name + " lost their last game" + " as " + temp_hero)
+
+    elif not isAccountId:
+        # Load User Data
+
+        vars_name = acctIdDb.getAllDocuments()
+        # Found User Bool
+        userFound = False
+
+        # User Variable
+        foundUser = None
+
+        # Loop through users
+        for item in vars_name:
+            if arg == item["name"]:
+                foundUser = item
+                userFound = True
+                break
+
+        user_id = foundUser["account_id"]
+        url = "https://api.opendota.com/api/players/" + str(user_id) + "/recentMatches"
+        #print(url)
+        recent = requests.get(url)
+        await ctx.send("getting recent matches...")
+        time.sleep(2)
+        json_data = json.loads(recent.text)
+        match_id = json_data[0]['match_id']
+        url = "https://api.opendota.com/api/matches/" + str(match_id)
+        await ctx.send("getting match...")
+        time.sleep(2)
+        data = requests.get(url)
+        json_data2 = json.loads(data.text)
+        players = json_data2["players"]
+        temp = None
+        temp_hero = None
+
+        for item in players:
+            if user_id == item["account_id"]:
+                temp = item
+        persona_name = temp["personaname"]
+        radiant_win = temp["radiant_win"]
+        is_radiant = temp["isRadiant"]
+        hero_id = temp["hero_id"]
+
+        get_hero = requests.get("https://api.opendota.com/api/heroes")
+        load_hero = json.loads(get_hero.text)
+
+        for item in load_hero:
+            if hero_id == item["id"]:
+                temp_hero = item["localized_name"]
+
+        if radiant_win:
+            if is_radiant:
+                await ctx.send(persona_name + " won their last game" + " as " + temp_hero)
+            else:
+                await ctx.send(persona_name + " lost their last game" + " as " + temp_hero)
+        elif not radiant_win:
+            if not is_radiant:
+                await ctx.send(persona_name + " won their last game" + " as " + temp_hero)
+            else:
+                await ctx.send(persona_name + " lost their last game" + " as " + temp_hero)
 
 @bot.command()
 async def adduser(ctx, user, acct_id):
@@ -35,81 +147,46 @@ async def adduser(ctx, user, acct_id):
     user['account_id'] = int(acct_id)
 
     # Append the user to the userData file
-
+    acctIdDb.insertOne(user)
 
     # Save the data
 
 
     await ctx.send("Successfully Added {}".format(username))
 
-def addMatchid(user , Match_id):
+
+@bot.command()
+async def addMatchid(ctx, acct_id , match_id):
+
+    acct_id = acct_id
+    match_id = match_id
+
+    lastMatch = {}
+    lastMatch['account_id'] = acct_id
+    lastMatch['match_id'] = int(match_id)
+
+    lastMatchDb.insertOne(lastMatch)
+
+    await  ctx.send("account id and match id saved")
+
+
+def addMatchidDB(user , Match_id):
     username = user
     match = Match_id
 
-    matchData = loadMatchid()
+
 
     lastMatch = {}
     lastMatch['account_id'] = username
     lastMatch['match_id'] = int(match)
 
-    matchData['Matches'].append(lastMatch)
-
-    saveMatchid(matchData)
-
-
-
-@bot.command()
-async def findUserByName(ctx, name):
-    # Load userData
-    userData = loadUsers()
-
-    # Found User Bool
-    userFound = False
-
-    # User Variable
-    foundUser = None
-
-    # Loop through users
-    for item in userData['users']:
-        if item['name'] == name:
-            foundUser = item
-            userFound = True
-            break
-
-    if userFound:
-        await ctx.send("User Found. Name: {}, Account Id: {}".format(foundUser['name'], foundUser['account_id']))
-    else:
-        await ctx.send("User Not Found")
-
-# Helper Functions
-def saveUsers(json_file):
-    with open('magic.txt', 'w') as outfile:
-        json.dump(json_file, outfile)
-
-
-def saveMatchid(json_file):
-    with open('matchid.txt', 'w') as outfile:
-        json.dump(json_file, outfile)
-
-def loadUsers():
-    temp = None
-    with open('magic.txt') as json_file:
-        data = json.load(json_file)
-        temp = data
-    return temp
-
-def loadMatchid():
-    temp = None
-    with open('matchid.txt') as json_file:
-        data = json.load(json_file)
-        temp = data
-    return temp
+    lastMatchDb.insertOne(lastMatch)
 
 @tasks.loop(seconds=10.0)
 async def slow_count():
-
-    userData = loadUsers()
-    matchData = loadMatchid()
+    
+    vars_user = acctIdDb.getAllDocuments()
+    vars_match = lastMatchDb.getAllDocuments()
 
     # Found User Bool
     userFound = False
@@ -117,31 +194,36 @@ async def slow_count():
     # User Variable
     foundUser = None
     # Loop through users
-    for user in userData['users']:
-        for match in matchData["Matches"]:
-            if match['account_id'] == user["account_id"]:
+    for user in vars_user:
+        for match in vars_match:
+            if match['account_id'] == str(user["account_id"]):
                 foundUser = user
                 userFound = True
                 break
+    #need to rewind mongo cursor after every use
+    vars_match.rewind()
+    vars_user.rewind()
 
     user_id = foundUser["account_id"]
+
     url = "https://api.opendota.com/api/players/" + str(user_id) + "/recentMatches"
     #print(url)
     recent = requests.get(url)
     #print("getting recent matches...")
-    time.sleep(2)
     json_data = json.loads(recent.text)
-    match_id = json_data[0]['match_id']
-
+    updated_match_id = json_data[0]['match_id']
     newMatch = None
-    for item in matchData['Matches']:
-        if match_id != item['match_id']:
-            item['match_id'] = match_id
+    foundmatch = False
+    for item in vars_match:
+        if updated_match_id != item['match_id']:
+            item['match_id'] = updated_match_id
+            foundmatch = True
             newMatch = item
-            saveMatchid(matchData)
-            url = "https://api.opendota.com/api/matches/" + str(match_id)
+            print(newMatch)
+            lastMatchDb.updateOneByAccountId(user_id, newMatch)
+            print("updated")
+            url = "https://api.opendota.com/api/matches/" + str(newMatch)
             print("getting match...")
-            time.sleep(2)
             data = requests.get(url)
             json_data2 = json.loads(data.text)
             players = json_data2["players"]
@@ -174,11 +256,14 @@ async def slow_count():
                 else:
                     print(persona_name + " lost their last game" + " as " + temp_hero)
             break
-        elif match_id == item['match_id']:
-            pass
+        else:
+            await channel.send("no new update")
+            break
 
+    #need to rewind mongo cursor after every use
+    vars_match.rewind()
+    vars_user.rewind()
 
 slow_count.start()
-
 
 bot.run(config.Bot_Token)
---
+++ mongoDriver.py
@@ -0,0 +1,31 @@
+import pymongo
+import pprint as pp
+from pymongo import MongoClient
+
+# Creates the MongoDB Creation 
+class mongoConnection:
+    
+    # Initalize the Mongo connection
+    def __init__(self, ip_address, db_name, coll_name, port='27017'):
+        connectionUrl = 'mongodb://{}:{}'.format(ip_address, port)
+        client = MongoClient(connectionUrl)
+        database = client[db_name]
+        self.db = database[coll_name]
+
+    # Insert one object and return ID.
+    def insertOne(self, object):
+        return self.db.insert_one(object).inserted_id
+
+    # Find first document with specified name
+    def findByName(self, object_name):
+        return self.db.find_one({ "name": object_name })
+
+    # Get all documents in database
+    def getAllDocuments(self):
+        return self.db.find()
+
+    # updates a current document
+    def updateOneByAccountId(self, account_id, object):
+        query = { "account_id": account_id }
+        update = { "$set": object }
+        self.db.update_one(query, update)
