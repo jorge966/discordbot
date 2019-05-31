a18328b72b034c78b2f173cd1823fe8a48664beb
Steve <steve@truu.id>
2019-05-31 11:58:37 -0400

    Adding on_join functions

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -1,7 +1,9 @@
 import botMain
 from discord.ext import commands
 import time
+import mongoDriver as md
 
+guildDb = md.mongoConnection("127.0.0.1", "Channels", "JoinedChannels")
 
 class Utilities(commands.Cog):
 
@@ -14,6 +16,15 @@ class Utilities(commands.Cog):
         current_time = (time.time() - botMain.Bot.start_time)/3600
         print('Bot has been running for: {0:.2f} hours'.format(current_time))
         await ctx.send('Bot has been running for: **{0:.2f}** hours'.format(current_time))
+
+    async def on_guild_join(self, guild):
+        guild = {}
+        guild['guild_id'] = guild.id
+        guild['guild_name'] = guild.name
+        guild['guild_owner_id'] = guild.owner_id
+        guild['base_text_channel'] = guild.text_channels[0]
+        guild['join_time'] = time.time()
+        guildDb.insertOne(guild)
 
 def setup(bot):
     util = Utilities(bot)
